839022770bf140096ebd83e9c89b394ecfe50cf0
fool <chrism@netlify.com>
2016-09-06 10:04:25 -0700

    test form

--- index.php
+++ index.php
@@ -5,3 +5,17 @@ print "<b>Date/Time (PST) Built: ".date("Ymd H:i:s e")." </b><p>\n";
 
 print "Local time from the browser: <br>"."<script src='date.js'></script>"
 ?>
+
+<form name="contact" action="index.html" netlify>
+  <p>
+    <label>Your Name:</label>
+    <input type="text" name="name">
+  </p>
+  <p>
+    <label>Your Email</label>
+    <input type="email" name="email">
+  </p>
+  <p>
+    <button>Send</button>
+  </p>
+</form>
